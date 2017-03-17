52caef2b2ee9fe4be2a4b3c1335ebac9e98e5780
Dan <daniel.sohval@gmail.com>
2017-03-17 12:06:28 -0400

    initial commit for 1702-gh

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'))
+
+app.use(function(err, req, res, next) {
+	console.error(err)
+	res.status(500)
+	res.send(err, err.stack)
+})
+
+app.listen(3000, function(){
+	console.log("Listening on 3000")
+})
