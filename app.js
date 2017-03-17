45c0cb0bf345dad21f2db868996d67588aa8fb93
Keziyah <keziyahlewis@gmail.com>
2017-03-17 13:07:25 -0400

    started wireframe

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ var morgan = require('morgan');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'))
 
 app.use(function(err, req, res, next) {
-	console.error(err)
-	res.status(500)
-	res.send(err, err.stack)
+    console.error(err)
+    res.status(500)
+    res.send(err, err.stack)
 })
 
-app.listen(3000, function(){
-	console.log("Listening on 3000")
+app.listen(3000, function() {
+    console.log("Listening on 3000")
 })
